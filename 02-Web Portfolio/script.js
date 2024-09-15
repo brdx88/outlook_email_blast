e1063234229f904fe2d1a8f894a76a42dd3b0120
Brian Ivan Cusuanto <brian.ic@outlook.com>
2024-09-15 22:43:39 +0700

    comment the hover gradient

--- 02-Web Portfolio/script.js
+++ 02-Web Portfolio/script.js
@@ -7,3 +7,14 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
+
+// // This script will handle the background hover effect
+// document.body.addEventListener('mousemove', function(e) {
+//     const x = e.clientX;
+//     const y = e.clientY;
+//     const lightSize = 200; // radius size of the light effect
+//     const gradientX = (x / window.innerWidth) * 100;
+//     const gradientY = (y / window.innerHeight) * 100;
+    
+//     document.body.style.background = `radial-gradient(circle at ${gradientX}% ${gradientY}%, rgba(17, 34, 64, 0.3), #0a192f ${lightSize}px)`;
+// });
